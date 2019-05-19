334ebccaa24541379a150478fec165a3b8bb0003
tejus <tejus.k.v@gmail.com>
2019-05-19 14:14:47 +0530

    Add tests for collatz of 0, 1, and 2 collatz(0) should throw an IllegalArgumentException

--- src/test/java/collatz/CollatzTest.java
+++ src/test/java/collatz/CollatzTest.java
@@ -1,4 +1,21 @@
 package collatz;
 
+import org.junit.Test;
+
 public class CollatzTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void collatzOfZeroShouldThrowIllegalArgumentException() {
+        Collatz.collatz(0);
+    }
+
+    @Test
+    public void collatzOfOneShouldReturnOne() {
+        assert Collatz.collatz(1) == 1;
+    }
+
+    @Test
+    public void collatzOfTwoShouldReturnOne() {
+        assert Collatz.collatz(2) == 1;
+    }
 }
