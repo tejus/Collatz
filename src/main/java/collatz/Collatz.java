f3bfbd6badb06f66f1a5ce0e4a797a9a63392639
tejus <tejus.k.v@gmail.com>
2019-05-19 14:13:36 +0530

    Add a collatz function that returns the final collatz result, ie ideally 1

--- src/main/java/collatz/Collatz.java
+++ src/main/java/collatz/Collatz.java
@@ -1,4 +1,17 @@
 package collatz;
 
 public class Collatz {
+
+    static int collatz(long number) {
+        if (number < 1) {
+            throw new IllegalArgumentException("Number needs to be greater than zero!");
+        }
+        if (number == 1) {
+            return 1;
+        } else if (number % 2 == 0) {
+            return collatz(number / 2);
+        } else {
+            return collatz(3 * number + 1);
+        }
+    }
 }
