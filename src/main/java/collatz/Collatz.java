a82a5866adcfe1955661944c9dc0d3849c3a7742
tejus <tejus.k.v@gmail.com>
2019-05-20 13:24:46 +0530

    Add a memoized version of the sequence length compute method

--- src/main/java/collatz/Collatz.java
+++ src/main/java/collatz/Collatz.java
@@ -36,4 +36,31 @@ public class Collatz {
         }
         return sequenceLengths;
     }
+
+    static int[] memoizedComputeSequenceLengths(final long n) {
+        //Let's throw an IllegalArgumentException if n is beyond the range
+        //of an integer in Java, since int arrays can't be initialised with long as size
+        if (n > Integer.MAX_VALUE) {
+            throw new IllegalArgumentException("Input number too long!");
+        }
+
+        int[] sequenceLengths = new int[((int) n) + 1];
+        for (int i = 1; i <= (int) n; i++) {
+            long currentN = i;
+            int count = 0;
+            do {
+                //Check if the current sequence length has been calculated already
+                if (currentN < i) {
+                    count += sequenceLengths[(int) currentN];
+                    break;
+                }
+                count++;
+                if (currentN == 1) break;
+                else if (currentN % 2 == 0) currentN /= 2;
+                else currentN = currentN * 3 + 1;
+            } while (true);
+            sequenceLengths[i] = count;
+        }
+        return sequenceLengths;
+    }
 }
